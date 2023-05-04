438b1956486b76d03ca9524c4d41761030eb414a
JuanCirera <cirerarosa@hotmail.es>
2023-05-04 12:25:00 +0000

    about done

--- resources/views/livewire/pages/about-us.blade.php
+++ resources/views/livewire/pages/about-us.blade.php
@@ -1,10 +1,26 @@
 <div>
-    <div class="container mt-8">
-        <h4 class="text-center text-primary">- VIDEO GAMES VAULT - </h4>
-        <p class="text-white">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat vero non assumenda excepturi molestias
-            aliquid nesciunt aspernatur architecto dolores nisi laudantium, necessitatibus provident ex sint asperiores
-            omnis ea repudiandae porro.
+    <div class="container mt-8 text-white w-md-70">
+        <h4 class="text-center text-gradient text-primary mb-4"> - VIDEO GAMES VAULT - </h4>
+        <p>
+            ¡Bienvenido a nuestra plataforma de videojuegos! Somos un equipo apasionado de desarrolladores y jugadores
+            que creemos que los videojuegos son más que solo un pasatiempo: son una forma de arte, una forma de
+            conectarnos con otros y una forma de explorar nuevas realidades.
+        </p>
+        <p>
+            Nuestra plataforma ha sido diseñada para que puedas guardar y clasificar tu biblioteca de videojuegos,
+            escribir reviews y seguir a otros usuarios para ver sus perfiles y descubrir nuevos títulos emocionantes. Ya
+            sea que estés buscando un juego de aventuras, un shooter en primera persona o un juego de rol épico, nuestra
+            plataforma tiene algo para todos.
+        </p>
+        <p>
+            Con nuestra plataforma, puedes conectarte con otros jugadores y construir una comunidad en línea de personas
+            apasionadas por los videojuegos. Comparte tus experiencias, descubre nuevos juegos y encuentra amigos con
+            intereses similares.
+        </p>
+        <p>
+            Estamos comprometidos con ofrecerte la mejor experiencia de usuario posible y nos esforzamos por mantener
+            nuestra plataforma actualizada con las últimas características y tecnologías. Si tienes alguna pregunta,
+            comentario o sugerencia, no dudes en ponerte en contacto con nosotros en cualquier momento.
         </p>
     </div>
     @include('layouts.footers.auth.footer')
