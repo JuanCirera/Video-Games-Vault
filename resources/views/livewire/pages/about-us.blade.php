1b9fe2eab39eef0e9634efe0c6b8108be91bb62b
JuanCirera <cirerarosa@hotmail.es>
2023-06-03 09:48:26 +0000

    FIX: about us view reworked

--- resources/views/livewire/pages/about-us.blade.php
+++ resources/views/livewire/pages/about-us.blade.php
@@ -1,29 +1,63 @@
 <div>
     <div class="container mt-8 text-white w-md-90 w-lg-60">
         <h4 class="text-center text-gradient text-primary mb-4"> - VIDEO GAMES VAULT - </h4>
-        <h5 class="text-primary">¿Quiénes somos?</h5>
-        <p>
-            ¡Bienvenido a nuestra plataforma de videojuegos! Somos un equipo apasionado de desarrolladores y jugadores
-            que creemos que los videojuegos son más que solo un pasatiempo: son una forma de arte, una forma de
-            conectarnos con otros y una forma de explorar nuevas realidades.
-        </p>
-        <h5 class="text-primary">Nuestro objetivo</h5>
-        <p>
-            Nuestra plataforma ha sido diseñada para que puedas guardar y clasificar tu biblioteca de videojuegos,
-            escribir reviews y seguir a otros usuarios para ver sus perfiles y descubrir nuevos títulos emocionantes. Ya
-            sea que estés buscando un juego de aventuras, un shooter en primera persona o un juego de rol épico, nuestra
-            plataforma tiene algo para todos.
-        </p>
-        <p>
-            Con nuestra plataforma, puedes conectarte con otros jugadores y construir una comunidad en línea de personas
-            apasionadas por los videojuegos. Comparte tus experiencias, descubre nuevos juegos y encuentra amigos con
-            intereses similares.
-        </p>
-        <p>
-            Estamos comprometidos con ofrecerte la mejor experiencia de usuario posible y nos esforzamos por mantener
-            nuestra plataforma actualizada con las últimas características y tecnologías. Si tienes alguna pregunta,
-            comentario o sugerencia, no dudes en ponerte en contacto con nosotros en cualquier momento.
-        </p>
+        <div class="card bg-gray-900">
+            <div class="card-header bg-gray-800 px-4 py-3">
+                <h5 class="text-primary m-0">¿Quiénes somos?</h5>
+            </div>
+            <div class="card-body">
+                <p>
+                    ¡Bienvenido a nuestra plataforma de videojuegos! Somos un equipo apasionado de desarrolladores y
+                    jugadores
+                    que creemos que los videojuegos son más que solo un pasatiempo: son una forma de arte, una forma de
+                    conectarnos con otros y una forma de explorar nuevas realidades.
+                </p>
+            </div>
+        </div>
+
+        <div class="card bg-gray-900 mt-4">
+            <div class="card-header bg-gray-800 px-4 py-3">
+                <h5 class="text-primary m-0">Nuestro objetivo</h5>
+            </div>
+            <div class="card-body">
+                <p>
+                    Nuestra plataforma ha sido diseñada para que puedas guardar y clasificar tu biblioteca de
+                    videojuegos,
+                    escribir reviews y seguir a otros usuarios para ver sus perfiles y descubrir nuevos títulos
+                    emocionantes. Ya
+                    sea que estés buscando un juego de aventuras, un shooter en primera persona o un juego de rol épico,
+                    nuestra
+                    plataforma tiene algo para todos.
+                </p>
+                <p>
+                    Con nuestra plataforma, puedes conectarte con otros jugadores y construir una comunidad en línea de
+                    personas
+                    apasionadas por los videojuegos. Comparte tus experiencias, descubre nuevos juegos y encuentra
+                    amigos
+                    con
+                    intereses similares.
+                </p>
+                <p>
+                    Estamos comprometidos con ofrecerte la mejor experiencia de usuario posible y nos esforzamos por
+                    mantener
+                    nuestra plataforma actualizada con las últimas características y tecnologías. Si tienes alguna
+                    pregunta,
+                    comentario o sugerencia, no dudes en ponerte en contacto con nosotros en cualquier momento.
+                </p>
+            </div>
+        </div>
+
+        <div class="card bg-gray-900 mt-4">
+            <div class="card-header bg-gray-800 px-4 py-3">
+                <h5 class="text-primary m-0"><i class="fa-brands fa-github"></i> Repositorio</h5>
+            </div>
+            <div class="card-body">
+                <p>
+                    <a href="https://github.com/JuanCirera/Video-Games-Vault" target="_blank">Video Games Vault -
+                        Github</a>
+                </p>
+            </div>
+        </div>
     </div>
     @include('layouts.footer')
 </div>
