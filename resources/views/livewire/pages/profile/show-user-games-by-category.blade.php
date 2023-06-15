901cd4bbd58eb5ac3ca34569cb28ab3bec116c7d
JuanCirera <cirerarosa@hotmail.es>
2023-06-15 14:58:17 +0000

    FIX: visual bug in user profile

--- resources/views/livewire/pages/profile/show-user-games-by-category.blade.php
+++ resources/views/livewire/pages/profile/show-user-games-by-category.blade.php
@@ -3,6 +3,7 @@
         @foreach ($user->videogames()->wherePivot('category', $category)->get() as $i => $categorizedGame)
             <article class="col-12 col-md-6 col-lg-4 px-md-2">
                 <div class="card bg-gray-800 mb-4">
+                    {{-- MOBILE --}}
                     <div class="row g-0 d-md-none">
                         <div class="col-4">
                             <img src="{{ $categorizedGame->image }}" alt=""
@@ -16,6 +17,7 @@
                             </div>
                         </div>
                     </div>
+                    {{-- DESKTOP --}}
                     <div class="d-none d-md-block">
                         <div>
                             <img src="{{ $categorizedGame->image }}" alt=""
@@ -24,29 +26,32 @@
                         <div class="card-body">
                             <div>
                                 <h4 class="card-title d-block text-white text-start px-0">
-                                    <a href="{{ route('game.show', $categorizedGame->slug) }}" class="link-white">{{ $categorizedGame->title }}</a>
+                                    <a href="{{ route('game.show', $categorizedGame->slug) }}"
+                                        class="link-white">{{ $categorizedGame->title }}</a>
                                 </h4>
                             </div>
                         </div>
                     </div>
-                    <div class="card-footer pt-2 pb-2 me-md-auto">
-                        <div class="dropdown">
-                            <button class="btn bg-gradient-primary dropdown-toggle w-100 w-md-auto" type="button" id="changeCategory"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Mover a...
-                            </button>
-                            <ul class="dropdown-menu w-100 w-md-auto text-center" aria-labelledby="changeCategory">
-                                @foreach ($categories as $cat)
-                                    <li>
-                                        <a class="dropdown-item"
-                                            wire:click="update('{{ $categorizedGame->id }}','{{ $cat->id }}')">
-                                            {{ ucfirst($cat->name) }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
+                    @if (Auth::user()->id == $user->id)
+                        <div class="card-footer pt-2 pb-2 me-md-auto">
+                            <div class="dropdown">
+                                <button class="btn bg-gradient-primary dropdown-toggle w-100 w-md-auto" type="button"
+                                    id="changeCategory" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Mover a...
+                                </button>
+                                <ul class="dropdown-menu w-100 w-md-auto text-center" aria-labelledby="changeCategory">
+                                    @foreach ($categories as $cat)
+                                        <li>
+                                            <a class="dropdown-item"
+                                                wire:click="update('{{ $categorizedGame->id }}','{{ $cat->id }}')">
+                                                {{ ucfirst($cat->name) }}
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
-                    </div>
+                    @endif
                 </div>
             </article>
         @endforeach
--- resources/views/livewire/pages/profile/user-tracking.blade.php
+++ resources/views/livewire/pages/profile/user-tracking.blade.php
@@ -9,7 +9,7 @@
                 {{-- @if ($trackedGame->followed) --}}
                 <div class="mb-4 col-md-6 col-lg-4 px-2">
                     <div class="card bg-gray-800">
-                        {{-- MOBILE CARD --}}
+                        {{-- MOBILE --}}
                         <div class="d-md-none">
                             <div class="row g-0">
                                 <div class="col-4">
@@ -25,55 +25,44 @@
                                         </a>
                                     </div>
                                     {{-- <div> --}}
-                                        {{-- <a class="mt-n-10" wire:click="stopTracking('{{ $trackedGame->id }}')">
+                                    {{-- <a class="mt-n-10" wire:click="stopTracking('{{ $trackedGame->id }}')">
                                         <i class="fas fa-bookmark text-primary text-2xl"></i>
                                     </a> --}}
                                     {{-- </div> --}}
                                 </div>
                             </div>
-                            <div class="mt-2 text-start text-center">
-                                <button class="btn btn-warning bg-gray-700 text-white w-90"
-                                    wire:click="stopTracking('{{ $trackedGame->id }}')">
-                                    <i class="far fa-circle-xmark"></i> Dejar de seguir
-                                </button>
-                            </div>
+                            @if (Auth::user()->id == $user->id)
+                                <div class="mt-2 text-start text-center">
+                                    <button class="btn btn-warning bg-gray-700 text-white w-90"
+                                        wire:click="stopTracking('{{ $trackedGame->id }}')">
+                                        <i class="far fa-circle-xmark"></i> Dejar de seguir
+                                    </button>
+                                </div>
+                            @endif
                         </div>
+                        {{-- DESKTOP--}}
                         <div class="d-none d-md-block">
                             <div class="">
                                 <img src="{{ $trackedGame->image }}" alt=""
-                                    class="img-fluid border-radius-top-start-md-lg border-radius-top-end-md-lg">
+                                    class="img-fluid border-radius-top-start-md-lg border-radius-top-end-md-lg min-height-250 max-height-250 w-100"
+                                    style="object-fit: cover;">
                             </div>
                             <div class="">
                                 <div class="card-body px-4 py-2 row flex-grow-1">
-                                    <h5 class="text-white text-start">{{ $trackedGame->title }}</h5>
-                                    <div class="mt-2 text-start">
-                                        <button class="btn btn-warning bg-gray-700 text-white"
-                                            wire:click="stopTracking('{{ $trackedGame->id }}')">
-                                            <i class="far fa-circle-xmark"></i> Dejar de seguir
-                                        </button>
-                                    </div>
+                                    <h5 class="text-white text-start py-2">{{ $trackedGame->title }}</h5>
+                                    @if (Auth::user()->id == $user->id)
+                                        <div class="mt-2 text-start">
+                                            <button class="btn btn-warning bg-gray-700 text-white"
+                                                wire:click="stopTracking('{{ $trackedGame->id }}')">
+                                                <i class="far fa-circle-xmark"></i> Dejar de seguir
+                                            </button>
+                                        </div>
+                                    @endif
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                {{-- @else
-                    <div class="mx-auto">
-                        <p>
-                            @if ($user->id == Auth::user()->id)
-                                Aún no tienes juegos juegos en seguimiento
-                                @php
-                                    break;
-                                @endphp
-                            @else
-                                <b>{{ $user->username }}</b> aun no tiene juegos en seguimiento
-                                @php
-                                    break;
-                                @endphp
-                            @endif
-                        </p>
-                    </div>
-                @endif --}}
             @endforeach
         </section>
     @else
